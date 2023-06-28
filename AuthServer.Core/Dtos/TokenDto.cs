d2156d443415a432eb7a9a4cf7d8b309906822ad
Taylan ALTUN <taylanaltun@gmail.com>
2023-06-28 08:54:08 +0300

    Create Client, ClientToken, TokenDto and ITokenService

---
+++ AuthServer.Core/Dtos/TokenDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Dtos
+{
+    public class TokenDto
+    {
+        public string AccessToken { get; set; }
+        public DateTime AccessTokenExpiration { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiration { get; set; }
+    }
+}
