2809de235b57a3d290ffba9c2db56de7c7fe7e48
Taylan ALTUN <taylanaltun@gmail.com>
2023-06-28 09:03:48 +0300

    Create IAuthenticationService, ClientLoginDto and LoginDto

---
+++ AuthServer.Core/Dtos/ClientLoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Dtos
+{
+    public class ClientLoginDto
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
---
+++ AuthServer.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,22 @@
+﻿using AuthServer.Core.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    internal interface IAuthenticationService
+    {
+        Task<ResponseDto<TokenDto>> CreateTokenAsync(LoginDto loginDto);
+
+        Task<ResponseDto<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
+
+        Task<ResponseDto<NoContentResult>> RevokeRefreshToken(string refreshToken);
+
+        Task<ResponseDto<ClientTokenDto>> CreateTokenByClient (ClientLoginDto clientLoginDto);
+    }
+}
