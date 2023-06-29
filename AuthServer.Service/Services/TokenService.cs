13c0d930e11ee68e9cf2421cc2921117e2601be8
Taylan ALTUN <taylanaltun@gmail.com>
2023-06-29 20:13:19 +0300

    Create TokenService

---
+++ AuthServer.Service/Services/TokenService.cs
@@ -0,0 +1,48 @@
+﻿using AuthServer.Core.Configurations;
+using AuthServer.Core.Dtos;
+using AuthServer.Core.Models;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using SharedLibrary.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Service.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly UserManager<UserApp> _userManager;
+        private readonly CustomTokenOption _customTokenOption;
+
+        public TokenService(UserManager<UserApp> userManager, IOptions<CustomTokenOption> option)
+        {
+            _userManager = userManager;
+            _customTokenOption = option.Value;
+        }
+
+        private string CreateRefreshToken()
+        {
+            var numberByte = new Byte[32];
+
+            using var rnd = RandomNumberGenerator.Create();
+            rnd.GetBytes(numberByte);
+
+            return Convert.ToBase64String(numberByte);
+        }
+        public ClientTokenDto CreateClientToken(Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TokenDto CreateToken(UserApp userApp)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
