e25d8006b838d67f1d2aa4ae968c58f33754b686
Taylan ALTUN <taylanaltun@gmail.com>
2023-06-29 09:47:53 +0300

    Create IUserService, IUnitOfWorf, UserAppDto and CreateUserDto

---
+++ AuthServer.Core/Services/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using AuthServer.Core.Dtos;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    public interface IUserService
+    {
+        Task<ResponseDto<UserAppDto>> CreateUserAsync(CreateUserDto createUserDto);
+
+        Task<ResponseDto<UserAppDto>> GetUSerByNameAsync(string userName);
+    }
+}
