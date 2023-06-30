6863932407d73faaa80f6a98109bb111cc93ee69
Taylan ALTUN <taylanaltun@gmail.com>
2023-06-30 10:38:45 +0300

    Create and implement AuthController

---
+++ AuthServer.API/Controllers/AuthController.cs
@@ -0,0 +1,51 @@
+﻿using AuthServer.Core.Dtos;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AuthServer.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AuthController : CustomBaseController
+    {
+        private readonly IAuthenticationService _authenticationService;
+
+        public AuthController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateToken (LoginDto loginDto)
+        {
+            var result= await _authenticationService.CreateTokenAsync(loginDto);
+
+            return ActionResultInstance(result);
+        }
+
+        [HttpPost]
+        public IActionResult CreateTokenByClient(ClientLoginDto loginDto)
+        {
+            var result=  _authenticationService.CreateTokenByClient(loginDto);
+
+            return ActionResultInstance(result);
+        }
+        [HttpPost]
+        public async Task<IActionResult> RevokeRefreshToken(RefreshTokenDto refreshTokenDto)
+        {
+            var result = await _authenticationService.RevokeRefreshToken(refreshTokenDto.Token);
+
+            return ActionResultInstance(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTokenByRefreshToken(RefreshTokenDto refreshTokenDto)
+        {
+            var result = await _authenticationService.CreateTokenByRefreshToken(refreshTokenDto.Token);
+
+            return ActionResultInstance(result);
+        }
+    }
+}
---
+++ AuthServer.API/Controllers/CustomBaseController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SharedLibrary.Dtos;
+
+namespace AuthServer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomBaseController : ControllerBase
+    {
+        [NonAction]
+        public IActionResult ActionResultInstance<T>(ResponseDto<T> response) where T : class 
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode,
+            };
+        }
+    }
+}
