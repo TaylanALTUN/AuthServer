7cd3933821c52a31e6b9232211d9b8d4704485aa
Taylan ALTUN <taylanaltun@gmail.com>
2023-06-30 11:25:59 +0300

    Create amd implement ProductController

---
+++ AuthServer.API/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using AuthServer.Core.Dtos;
+using AuthServer.Core.Models;
+using AuthServer.Core.Repositories;
+using AuthServer.Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AuthServer.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : CustomBaseController
+    {
+        private readonly IServiceGeneric<Product,ProductDto> _productService;
+
+        public ProductController(IServiceGeneric<Product, ProductDto> productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult>  GetProducts()
+        {
+            return ActionResultInstance(await _productService.GetAllAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveProduct (ProductDto productDto)
+        {
+            return ActionResultInstance(await _productService.AddAsync(productDto));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct(ProductDto productDto)
+        {
+            return ActionResultInstance(await _productService.Update(productDto,productDto.Id));
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeletePorduct (int id)
+        {
+            return ActionResultInstance(await _productService.Remove(id));
+        }
+    }
+}
