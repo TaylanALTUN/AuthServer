221a93e962eabf988bc8b95af2a54ea927692fb8
Taylan ALTUN <taylanaltun@gmail.com>
2023-06-30 11:03:42 +0300

    Bugfix UserAppDto

--- AuthServer.Core/Dtos/UserAppDto.cs
+++ AuthServer.Core/Dtos/UserAppDto.cs
@@ -8,7 +8,7 @@ namespace AuthServer.Core.Dtos
 {
     public class UserAppDto
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
         public string City { get; set; }
